1aed0e995a65b4c1671f782432e333db02b86cb7
atdaunais <atdaunais@gmail.com>
2019-08-29 10:32:30 -0500

    created add and delete task functions

---
+++ index2.py
@@ -0,0 +1,15 @@
+empty_list = []
+
+
+def add_task(any_list):
+    user_add_task = input("Enter a task you would like to add to the list: ")
+    any_list.append(user_add_task)
+
+
+def delete_task(any_list):
+    user_del_task = input("Enter a task you would like to delete from the list: ")
+    if user_del_task in any_list:
+        del_index = any_list.index(user_del_task)
+        del any_list[del_index]
+
+
